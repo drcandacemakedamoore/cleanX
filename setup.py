e1c9452d52f1954758125fd5b58cd3d0937b0386
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2021-04-12 12:24:45 +0300

    Create setup.py
    setup.py file, dependencies need to be added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+
+
+setup(
+    name="cleanX",
+    version='0.0.1',
+    description="Python library for cleaning data in large datasets of Xrays",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='doctormakeda@gmail.com',
+    author_email='doctormakeda@gmail.com',
+    maintainer='doctormakeda@gmail.com',
+    maintainer_email= 'doctormakeda@gmail.com',
+    url="https://github.com/drcandacemakedamoore/cleanX",
+    license="MIT",
+    py_modules=["cleanX"],
+    install_requires=["pip"]
+    
+)
