45dd856a3affe989c8b4ebecde7b41b7cbc301b1
Oleg Sivokon <olegsivokon@gmail.com>
2021-04-22 21:49:36 +0300

    Run code generation inside Sphinx configuration...

--- source/conf.py
+++ source/conf.py
@@ -53,3 +53,24 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+def on_builder_init(app):
+    import os
+    from sphinx.ext.apidoc import main
+
+    print('on_builder_init: {}'.format(app))
+    project_dir = os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__)),
+    )
+    main([
+        '-o', os.path.join(project_dir, 'source'),
+        '-f',
+        project_dir,
+        '.*',
+        'setup.py',
+    ])
+
+
+def setup(app):
+    app.connect('builder-inited', on_builder_init)
