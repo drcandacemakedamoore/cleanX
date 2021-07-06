0e0243c8b176dab1f5d88606be2755a16397f0c6
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2021-07-06 09:29:44 +0300

    pulling junk

--- cleanx/eda_images.py
+++
@@ -1,9 +0,0 @@
-# future place of code.
-## included images in this module should be:
-
-
-#1. avg_image_maker(set_of_images):
-
-#2. avg_image_maker_by_label
-#3. set_image_variability (variability image)
-   
