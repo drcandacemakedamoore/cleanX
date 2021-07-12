8e272e34613f7efa1787adad12db5b0a2e9702d6
Oleg Sivokon <olegsivokon@gmail.com>
2021-07-12 19:06:32 +0300

    [fix] linting errors

--- cleanX/dicom_processing/__init__.py
+++ cleanX/dicom_processing/__init__.py
@@ -3,11 +3,18 @@
 import warnings
 from textwrap import dedent
 
+from .source import (
+    DirectorySource,
+    GlobSource,
+    MultiSource,
+)
+
+
 HAS_PYDICOM = False
 HAS_SIMPLEITK = False
 
 try:
-    import pydicom
+    import pydicom  # noqa: E402
     from .pydicom_adapter import (
         PydicomDicomReader as DicomReader,
         get_jpg_with_pydicom as rip_out_jpgs,
@@ -18,7 +25,7 @@ except ModuleNotFoundError:
 
 if not HAS_PYDICOM:
     try:
-        import SimpleITK
+        import SimpleITK  # noqa: E402
         from .simpleitk_adapter import (
             rip_out_jpgs_sitk as rip_out_jpgs,
         )
@@ -27,17 +34,13 @@ if not HAS_PYDICOM:
         pass
 
 if not (HAS_SIMPLEITK or HAS_PYDICOM):
-    warnings.warn(dedent(
-        '''
-        Neither SimpleITK nor PyDICOM are installed.
+    warnings.warn(
+        dedent(
+            '''
+            Neither SimpleITK nor PyDICOM are installed.
 
-        Will not be able to extract information from DICOM files.
-        ''',
+            Will not be able to extract information from DICOM files.
+            ''',
+        ),
         UserWarning,
-    ))
-
-from .source import (
-    DirectorySource,
-    GlobSource,
-    MultiSource,
-)
+    )
