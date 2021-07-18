8a223d3c922127a9c5d4dfbf89b1b35ae72e92d0
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2021-07-18 15:02:20 +0300

    relinting new branch

--- cleanX/dataset_processing/dataframes.py
+++ cleanX/dataset_processing/dataframes.py
@@ -19,15 +19,15 @@ class MLSetup:
         # TODO: Implement
         self.train_df = train_df
         self.test_df = test_df
-        
+
     # def train_maker():
 
     def metadata(self):
         return self.train_df.columns, self.test_df.columns
-    
+
     def concat_dataframe(self):
         return self.train_df.concatenate(self.test_df)
-    
+
     def duplicated(self):
         return self.concat_dataframe().duplicated()
 
