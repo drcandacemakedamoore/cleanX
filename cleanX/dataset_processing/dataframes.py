95dda21ac8275b1e77eb7f6f84d1a4bb489e9ba6
Oleg Sivokon <olegsivokon@gmail.com>
2021-08-04 23:27:33 +0300

    More documentation for dataframes

--- cleanX/dataset_processing/dataframes.py
+++ cleanX/dataset_processing/dataframes.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-clean X : Library for cleaning radiological data used in machine learning
+Library for cleaning radiological data used in machine learning
 applications.
+
 Module dataframes: a module for processing of datasets related to images.
 This module can be implemented by functions,
 or can be implemented with classes.
@@ -12,7 +13,7 @@ import html
 import logging
 import re
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from collections.abc import Iterable, Sequence
 from pathlib import Path
 
@@ -22,12 +23,28 @@ import textwrap as tw
 
 
 class GuesserError(TypeError):
+    """
+    This error is raised when the loading code cannot figure
+    out what kind of source it is dealing with.
+
+    """
     pass
 
 
 class ColumnsSource(ABC):
+    """
+    Formal superclass for all sources that should be used to
+    produce :class:`~pandas.DataFrame`.
+    """
 
+    @abstractmethod
     def to_dataframe(self):
+        """
+        Descendants of this class must implement this method.
+
+        :return: Dataframe produced from the source represented by this class.
+        :rtype: :class:`~pandas.DataFrame`.
+        """
         raise NotImplementedError()
 
 
@@ -35,43 +52,117 @@ class CSVSource(ColumnsSource):
     """Class that helps turn csv into a dataframe"""
 
     def __init__(self, csv, **pd_args):
+        """
+        Initializes this class with the path to the csv file from which
+        to create the dataframe and the pass-through arguments to
+        :func:`pandas.read_csv()`.
+
+        :param csv: The path to CSV file or an open file handle.  Must be
+                    suitable for :func:`pandas.read_csv()`.
+        :param \\**pd_args: The pass-through arguments to
+                            :func:`pandas.read_csv()`.
+        """
         self.csv = csv
         self.pd_args = pd_args or {}
 
     def to_dataframe(self):
+        """
+        Necessary implementation of the abstractmethod.
+
+        :return: Dataframe produced from the source represented by this class.
+        :rtype: :class:`~pandas.DataFrame`.
+        """
         return pd.read_csv(self.csv, **self.pd_args)
 
 
 class JSONSource(ColumnsSource):
     """Class that helps turn json into a dataframe for later exploration"""
+
     def __init__(self, json, **pd_args):
+        """
+        Initializes this class with the path to the json file from which
+        to create the dataframe and the pass-through arguments to
+        :func:`pandas.read_json()`.
+
+        :param json: The path to JSON file or an open file handle.  Must be
+                    suitable for :func:`pandas.read_json()`.
+        :param \\**pd_args: The pass-through arguments to
+                            :func:`pandas.read_json()`.
+        """
         self.json = json
         self.pd_args = pd_args or {}
 
     def to_dataframe(self):
+        """
+        Necessary implementation of the abstractmethod.
+
+        :return: Dataframe produced from the source represented by this class.
+        :rtype: :class:`~pandas.DataFrame`.
+        """
         return pd.read_json(self.json, **self.pd_args)
 
 
 class DFSource(ColumnsSource):
-    """Class initializes dataframes for later exploration and reporting"""
+    """
+    This class is a no-op source.  Use this when you already have a dataframe
+    ready.
+    """
+
     def __init__(self, df):
+        """
+        Initializes this class with the existing dataframe.
+
+        :param df: Existing dataframe that should be used as the source.
+        :type df: :class:`~pandas.DataFrame`.
+        """
         self.df = df
 
     def to_dataframe(self):
+        """
+        Necessary implementation of the abstractmethod.
+
+        :return: Dataframe produced from the source represented by this class.
+        :rtype: :class:`~pandas.DataFrame`.
+        """
         return self.df
 
 
 class MultiSource(ColumnsSource):
+    """
+    This class allows aggregation of multiple sources.
+    """
 
     def __init__(self, *sources):
+        """
+        Initializes this class with the number of sources that will
+        be used in a way similar to :func:`itertools.chain()`.
+
+        :param \\*sources: Sources to be concatendated together to
+                           create a single dataframe.
+        """
         self.sources = sources
 
     def to_dataframe(self):
+        """
+        Necessary implementation of the abstractmethod.
+
+        :return: Dataframe produced from the source represented by this class.
+        :rtype: :class:`~pandas.DataFrame`.
+        """
         return pd.concat(s.to_dataframe() for s in self.sources)
 
 
 def string_source(raw_src):
-    """Class that helps process strings for later exploration"""
+    """
+    Helper function to select source based on file extension.
+
+    :param raw_src: The path to the file to be interpreted as a source.
+    :type raw_src: Suitable for :func:`os.path.splitext()`
+
+    :return: Either :class:`~.CSVSource` or :class:`~.JSONSource`, depending
+             on file extension.
+    :rtype: Union[CSVSource, JSONSource]
+    """
     ext = os.path.splitext(raw_src)[1]
     if isinstance(ext, bytes):
         ext = ext.decode()
@@ -87,9 +178,8 @@ def string_source(raw_src):
 class MLSetup:
     """
     This class allows configuration of the train and test datasets
-    organized into a pandas dataframe
-    to be checked for problems, and creates reports, which can be
-    put in multiple output options.
+    organized into a pandas dataframe to be checked for problems, and
+    creates reports, which can be put in multiple output options.
     """
 
     known_sources = {
@@ -98,6 +188,10 @@ class MLSetup:
         Path: string_source,
         pd.DataFrame: lambda _: DFSource,
     }
+    """
+    Mapping of types of sources to the factory functions for creating
+    source objects.
+    """
 
     def __init__(
             self,
@@ -107,7 +201,35 @@ class MLSetup:
             label_tag='Label',
             sensitive_list=None,
     ):
-        # TODO: Implement
+        """
+        Initializes this class with various aspects of a typical
+        machine-learning study.
+
+        :param train_src: The source for training dataset.
+        :type train_src: If this is a path-like object, it is interpreted
+                         to be a path to a file that needs to be read into
+                         a :class:`~pandas.DataFrame`.  If this is already
+                         a dataframe, it's used as is.  If this is an iterable
+                         it is interpreted as a sequence of different sources
+                         and will be processed using :class:`~.MultiSource`.
+        :param test_src: Similar to :code:`train_src`, but for testing dataset.
+        :type test_src: Same as :code:`train_src`
+        :param unique_id: The name of the column that uniquely identifies the
+                          cases (typically, this is patient's id).
+        :type unique_id: Suitable for accessing columns in :mod:`pandas`
+                         dataframe.
+        :param label_tag: Usually, the training dataset has an assesment column
+                          that assings the case to the category of interest.
+                          Typically, this is diagnosis, or finding, etc.
+        :type label_tag: Suitable for accessing columns in :mod:`pandas`
+                         dataframe.
+        :param sensitive_list: The list of columns that you suspect might be
+                               affecting the fairness of the study.  These
+                               are typically gender, age, ethnicity etc.
+        :type sensitive_list: A sequence of regular expression that will be
+                              applied to the colum names (converted to strings
+                              if necessary).
+        """
         self.train_src = self.guess_source(train_src)
         self.test_src = self.guess_source(test_src)
         self.unique_id = unique_id
@@ -115,6 +237,18 @@ class MLSetup:
         self.sensitive_list = sensitive_list
 
     def get_unique_id(self):
+        """
+        Tries to find the column in training and testing datasests that
+        uniquely identifies the entires in both.  Typically, this is
+        patient id of some sort.  If the setup was initialized with
+        :code:`unique_id`, than that is used.  Otherwise, a rather simple
+        heuristic is used.
+
+        :return: The name of the column that should uniquely identify
+                 the cases being studied.
+        :rtype: The value from :attr:`~pandas.DataFrame.columns`.  Typically,
+                this is a :class:`str`.
+        """
         if self.unique_id:
             return self.unique_id
         c1, c2 = self.metadata()
@@ -126,6 +260,11 @@ class MLSetup:
         return c1[0]
 
     def get_sensitive_list(self):
+        """
+        Returns a list of regular expressions that will be applied to the
+        list of columns to identify the senistive categories (those that
+        might bias the training towards overrepresented categories).
+        """
         # TODO(wvxvw): Try to come up with names that might catch some
         # sensitive category. The names will be interpreted as regular
         # expressions to match against column names
@@ -136,6 +275,17 @@ class MLSetup:
         ]
 
     def guess_source(self, raw_src):
+        """
+        Helper method to conver sources given by external factors to internal
+        representation.
+
+        :param raw_src: The externally supplied source.  This is typcially
+                        either a path to a file, or an existing dataframe
+                        or a collection of such sources.
+
+        :return: An internal representation of source.
+        :rtype: :class:`~.ColumnSource`
+        """
         guesser = self.known_sources.get(type(raw_src))
         if guesser:
             return guesser(raw_src)
@@ -145,21 +295,46 @@ class MLSetup:
             return raw_src
 
     def metadata(self):
+        """
+        Returns a tuple of column names of train and test datasets.
+
+        :return: Column names of train and test datasets.
+        """
         return (
             self.train_src.to_dataframe().columns,
             self.test_src.to_dataframe().columns,
         )
 
     def concat_dataframe(self):
+        """
+        Helper method to generate the dataset containing both training and
+        test data.
+
+        :returns: A combined dataframe.
+        :rtype: :class:`~pandas.DataFrame`
+        """
         return pd.concat((
             self.train_src.to_dataframe(),
             self.test_src.to_dataframe(),
         ))
 
     def duplicated(self):
+        """
+        Provides information on duplicates found in training and test data.
+
+        :return: A dataframe with information about duplicates.
+        :rtype: :class:`~pandas.DataFrame`
+        """
         return self.concat_dataframe().duplicated()
 
     def duplicated_frame(self):
+        """
+        Provides more detailed information about the ducplicates found in
+        training and test data.
+
+        :return: A tuple of two dataframes first listing duplicates in
+                 training data, second listing duplicates in test data.
+        """
         train_df = self.train_src.to_dataframe()
         test_df = self.test_src.to_dataframe()
         train_dupe_names = train_df[train_df.duplicated()]
@@ -168,17 +343,31 @@ class MLSetup:
         return (
             train_dupe_names,
             test_dupe_names,
-
-
         )
 
     def duplicates(self):
+        """
+        Calculates the number of duplicates in training and test datasets
+        separately.
+
+        :return: A tuple with number of duplicates in training and test
+                 datasets.
+        :rtype: Tuple[int, int]
+        """
         return (
             self.train_src.to_dataframe().duplicated().sum(),
             self.test_src.to_dataframe().duplicated().sum(),
         )
 
     def pics_in_both_groups(self, unique_id):
+        """
+        Generates dataframe listing cases that appear in both training and
+        testing datasets.
+
+        :return: A dataframe with images found in both training and testing
+                 datasets.
+        :rtype: :class:`~pandas.DataFrame`
+        """
         train_df = self.train_src.to_dataframe()
         test_df = self.test_src.to_dataframe()
         return train_df.merge(test_df, on=unique_id, how='inner')
@@ -190,6 +379,25 @@ class MLSetup:
         bias=True,
         understand=True,
     ):
+        """
+        Generates report object summarizing the properties of this setup.
+        This report can later be used to produce formatted output for
+        inspection by human.
+
+        :param duplicates: Whether the information on duplicates needs to
+                           be included in the report.
+        :type duplicates: bool
+        :param leakage: Whether the information on data leakage (cases from
+                        training set appearing in test set) should be
+                        included in report.
+        :type leakage: bool
+        :param bias: Whether the information about distribution in sensitive
+                     categories should be included in the report.
+        :type bias: bool
+        :param understand: Whether information about general properties of
+                           the dataset should be included in the report.
+        :type understand: bool
+        """
         return Report(
             self,
             duplicates,
@@ -231,6 +439,7 @@ class MLSetup:
 
 class Report:
     """This class is for a report which can be produced about the data"""
+
     def __init__(
         self,
         mlsetup,
